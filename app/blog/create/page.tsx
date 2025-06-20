1f66e9fdfeca1d6a14ee43fdf25fdc8f3b70095d
jinjuseo <wjbbdq1223@naver.com>
2025-06-20 17:39:20 +0900

    fix: moveButton

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -4,16 +4,28 @@ import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
 import { codeBlock } from "@blocknote/code-block";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useTheme } from "next-themes";
+import { BlockNoteSchema, defaultStyleSpecs } from "@blocknote/core";
 const page = () => {
+  const { theme } = useTheme();
+  const [title, setTitle] = useState("");
   const editor = useCreateBlockNote({
     codeBlock,
+    // defaultStyles,
   });
   return (
     <section className="min-h-screen bg-white dark:bg-slate-900 pt-24 pb-20">
       <p className="text-3xl text-black dark:text-white flex justify-center items-center">
         개발중...
       </p>
-      <BlockNoteView editor={editor} />
+      <Input placeholder="제목" />
+      <BlockNoteView
+        data-theming-css-demo
+        editor={editor}
+        theme={theme === "dark" ? "dark" : "light"}
+      />
     </section>
   );
 };
--- components/scroll-button.tsx
+++ components/scroll-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
 import { ChevronDown, ChevronUp } from "lucide-react";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 interface prop {
@@ -16,54 +16,60 @@ const MoveButton = ({
   isLastSection,
   sections,
 }: prop) => {
-  {
-    return createPortal(
-      <motion.div
-        animate={{ y: [0, 10, 0] }}
-        transition={{ duration: 2, repeat: Number.POSITIVE_INFINITY }}
-        className="fixed bottom-6 inset-x-0 mx-auto w-fit z-10"
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // 브라우저 환경일 때만 true
+  }, []);
+
+  if (!isClient) return null;
+
+  return createPortal(
+    <motion.div
+      animate={{ y: [0, 10, 0] }}
+      transition={{ duration: 2, repeat: Number.POSITIVE_INFINITY }}
+      className="fixed bottom-6 inset-x-0 mx-auto w-fit z-10"
+    >
+      <button
+        className={`bg-transparent p-3 duration-300 `}
+        onClick={() =>
+          setActiveSection((prev) => {
+            {
+              const currIndex = sections.indexOf(prev);
+              const nextIndex = (currIndex + 1) % sections.length;
+              scrollToSection(sections[nextIndex]);
+              return sections[nextIndex];
+            }
+          })
+        }
       >
-        <button
-          className={`bg-transparent p-3 duration-300 `}
-          onClick={() =>
-            setActiveSection((prev) => {
-              {
-                const currIndex = sections.indexOf(prev);
-                const nextIndex = (currIndex + 1) % sections.length;
-                scrollToSection(sections[nextIndex]);
-                return sections[nextIndex];
-              }
-            })
-          }
-        >
-          <AnimatePresence mode="wait">
-            {isLastSection ? (
-              <motion.div
-                key="up"
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                transition={{ duration: 0.3 }}
-              >
-                <ChevronUp className="w-8 h-8 text-slate-400" />
-              </motion.div>
-            ) : (
-              <motion.div
-                key="down"
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 10 }}
-                transition={{ duration: 0.3 }}
-              >
-                <ChevronDown className="w-8 h-8 text-slate-400" />
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </button>
-      </motion.div>,
-      document.body
-    );
-  }
+        <AnimatePresence mode="wait">
+          {isLastSection ? (
+            <motion.div
+              key="up"
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.3 }}
+            >
+              <ChevronUp className="w-8 h-8 text-slate-400" />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="down"
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 10 }}
+              transition={{ duration: 0.3 }}
+            >
+              <ChevronDown className="w-8 h-8 text-slate-400" />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </button>
+    </motion.div>,
+    document.body
+  );
 };
 
 export default MoveButton;
